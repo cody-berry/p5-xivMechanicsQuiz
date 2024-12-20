116dc241e3ec05a216083a20b262ee5a66775ecf
cody-berry <cody.tian@icloud.com>
2024-12-19 20:14:08 -0800

    Create all the windows needed

--- sketch.js
+++ sketch.js
@@ -2,14 +2,26 @@
  *  @author
  *  @date 2024.
  *
- */ import {CENTER} from "./lib/p5";
+ */
 
+// initial variables from template
 let font
 let fixedWidthFont
 let variableWidthFont
 let instructions
 let debugCorner /* output debug text in the bottom left corner of the canvas */
 
+// displaying the windows
+let topSquareSize = 100 // the size of the top corner squares
+let debugCornerSize = 61 // the height of the debug corner
+let topWidth = 500  // the width of the window at the top, not including the top corner squares
+let mechanicSelectionRows = 4 // The number of rows in "mechanic selection"
+let mechanicSelectionHeight = mechanicSelectionRows*30 // each row should take...50 height? I'm not sure
+let middleTopHeight = 100 // the height of the window just above the main body
+let mainBodyHeight = 400 // the height of the main window
+let bottomHeight = 100 // the height of the window at the bottom
+let holeSize = 20
+let cornerRounding = 10
 
 function preload() {
     font = loadFont('data/consola.ttf')
@@ -19,7 +31,8 @@ function preload() {
 
 
 function setup() {
-    let cnv = createCanvas(800, 1000)
+    let cnv = createCanvas(topSquareSize*2 + holeSize*2 + topWidth,
+        topSquareSize + mechanicSelectionHeight + middleTopHeight + mainBodyHeight + bottomHeight + debugCornerSize + holeSize*5)
     cnv.parent('#canvas')
     colorMode(HSB, 360, 100, 100, 100)
     textFont(font, 14)
@@ -36,19 +49,67 @@ function setup() {
 function draw() {
     background(234, 34, 24)
 
+
+    // erase the background afterward
     erase()
-    rect(0, 200, width, 210)
+    rect(0, 0, width, height)
     noErase()
 
-    /* debugCorner needs to be last so its z-index is highest */
+    // the green square at the top-left TODO
+    fill(120, 80, 50)
+    noStroke()
+    rect(0, 0, topSquareSize, topSquareSize, cornerRounding)
+
+    // the top window TODO
+    fill(234, 34, 24)
+    noStroke()
+    rect(topSquareSize + holeSize, 0, width - topSquareSize - holeSize,
+        topSquareSize, cornerRounding)
+
+    // the red square at the top-right TODO
+    fill(350, 80, 50)
+    noStroke()
+    rect(width - topSquareSize, 0,
+        width, topSquareSize, cornerRounding)
+
+    // the mechanic section window TODO
+    fill(234, 34, 24)
+    noStroke()
+    rect(0, topSquareSize + holeSize,
+        width, topSquareSize + mechanicSelectionHeight + holeSize, cornerRounding)
+
+    // the middle-top window TODO
+    fill(234, 34, 24)
+    noStroke()
+    rect(0, topSquareSize + mechanicSelectionHeight + holeSize*2,
+        width, topSquareSize + mechanicSelectionHeight + middleTopHeight + holeSize*2, cornerRounding)
+
+    // the main body window TODO
+    fill(234, 34, 24)
+    noStroke()
+    rect(0, topSquareSize + mechanicSelectionHeight + middleTopHeight + holeSize*3,
+        width, height - debugCornerSize - bottomHeight - holeSize*2, cornerRounding)
+
+    // the bottom window TODO
+    fill(234, 34, 24)
+    noStroke()
+    rect(0, height - debugCornerSize - bottomHeight - holeSize, width,
+        height - debugCornerSize - holeSize, cornerRounding)
+
+    // the debug corner window
+    fill(0, 0, 0)
+    noStroke()
+    rect(0, height - debugCornerSize, width, height, cornerRounding)
+    displayDebugCorner()
+}
+
+// since all the other things that display something on top of the separate
+// sections are in functions, this should be in a function too for consistency
+function displayDebugCorner() {
     debugCorner.setText(`frameCount: ${frameCount}`, 2)
     debugCorner.setText(`fps: ${frameRate().toFixed(0)}`, 1)
     debugCorner.showBottom()
-
-    if (frameCount > 3000)
-        noLoop()
 }
-
 
 function keyPressed() {
     /* stop sketch */
